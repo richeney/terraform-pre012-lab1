a4550f215d294b201b3ed83c26b71eb02728c36f
Richard Cheney <richard.cheney@microsoft.com>
2018-05-15 17:38:10 +0100

    FInal file

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "lab1" {
+  name     = "terraformLab1"
+  location = "West Europe"
+
+  tags {
+    environment = "Training"
+  }
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "richeneysa1976"
+  resource_group_name      = "${azurerm_resource_group.lab1.name}"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
