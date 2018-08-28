209d8c09b5374ec6523dc9f257f1312f957820f1
Richard Cheney <richard.cheney@microsoft.com>
2018-08-28 15:53:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "basics" {
-  name     = "terraformbasics"
+resource "azurerm_resource_group" "lab1" {
+  name     = "terraform-lab1"
   location = "West Europe"
 
   tags {
@@ -7,9 +7,9 @@ resource "azurerm_resource_group" "basics" {
   }
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "richeneysa1976"
-  resource_group_name      = "${azurerm_resource_group.basics.name}"
+resource "azurerm_storage_account" "lab1sa" {
+  name                     = "richeneyterraformlab1"
+  resource_group_name      = "${azurerm_resource_group.lab1.name}"
   location                 = "westeurope"
   account_tier             = "Standard"
   account_replication_type = "LRS"
