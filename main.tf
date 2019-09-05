a6870e871c9b76bb70de8965728db5936ce2f393
Richard Cheney <richeney@microsoft.com>
2019-09-05 16:07:01 +0100

    Terraform 0.12 compliant

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
+provider "azurerm" {
+  version = "~> 1.33.1"
+}
+
 resource "azurerm_resource_group" "lab1" {
   name     = "terraform-lab1"
   location = "West Europe"
-
-  tags {
+  tags = {
     environment = "training"
   }
 }
 
 resource "azurerm_storage_account" "lab1sa" {
-  name                     = "richeneyterraformlab1"
-  resource_group_name      = "${azurerm_resource_group.lab1.name}"
+  name                     = "richeneylab1sa"
+  resource_group_name      =  azurerm_resource_group.lab1.name
   location                 = "westeurope"
   account_tier             = "Standard"
   account_replication_type = "LRS"
